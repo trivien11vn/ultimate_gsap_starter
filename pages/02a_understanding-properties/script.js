dcb6b66f4b3c516f565f76d82d932988c8b2acd8
trivien11vn <triviencoderr@gmail.com>
2025-10-15 21:54:55 +0700

    add animation to box using GSAP

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,13 @@
+import gsap from "gsap";
+
+gsap.to('.box', {
+    opacity: 1,
+    rotation: 360,
+    background: '#ff6f61',
+    borderRadius: '50%',
+    scale: 1.25,
+    duration: 2,
+    yoyo: true,
+    repeat: -1,
+    ease: 'power1.inOut'
+})
