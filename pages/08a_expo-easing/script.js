e70f2721ca8699962e9438cf055fccf7fcb98b5e
trivien11vn <triviencoderr@gmail.com>
2025-10-17 23:10:02 +0700

    expo easing

--- pages/08a_expo-easing/script.js
+++ pages/08a_expo-easing/script.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 // Select the main floating action button (FAB)
 const fab = document.querySelector(".fab");
 
@@ -30,10 +32,27 @@ fab.addEventListener("click", () => {
       const y = -Math.sin(angle) * radius;
 
       // 🔜 Animation will go here
+      gsap.to(child, {
+        x: x,
+        y: y,
+        opacity: 1,
+        rotate: 360,
+        duration: 0.5,
+        ease: "expo.out" // start fast, end slow
+      })
     });
   } else {
     children.forEach((child) => {
       // 🔜 Collapse animation will go here
+      gsap.to(child, {
+        x: 0,
+        y: 0,
+        opacity: 0,
+        scale: 0.8,
+        rotate: 0,
+        ease: "power1.in",
+        pointerEvents: "none"
+      })
     });
   }
 });
